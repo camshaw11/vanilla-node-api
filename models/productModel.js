0db2e9ed603ffb9146b95d128811f574b0625ff0
cameron <cameronshaw95@gmail.com>
2020-10-30 00:00:04 +1300

    create write data to file function

--- models/productModel.js
+++ models/productModel.js
@@ -1,6 +1,8 @@
 const products = require('../data/products')
 const { v4: uuidv4 } = require('uuid')
 
+const { writeDataToFile } = require('../utils')
+
 function findAll() {
     return new Promise((resolve, reject) => {
         resolve(products)
@@ -17,6 +19,8 @@ function findById(id) {
 function create(product) {
     return new Promise((resolve, reject) => {
         const newProduct = {id: uuidv4(), ...product}
+        products.push(newProduct)
+        writeDataToFile('./data/products.json')
     })
 }
 
